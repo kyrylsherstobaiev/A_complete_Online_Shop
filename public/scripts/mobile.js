e5289722ed47bc6243dec9911833a77fcaa0e886
kyrylsherstobaiev <novemberua@gmail.com>
2023-01-25 16:24:14 +0200

    Frontend Toggling The Mobile Menu

---
+++ public/scripts/mobile.js
@@ -0,0 +1,9 @@
+const mobileMenuBtnElement = document.getElementById('mobile-menu-button');
+const mobileMenuElement = document.getElementById('mobile-menu');
+
+function toggleMobileMenu() {
+    mobileMenuElement.classList.toggle('open');
+}
+
+mobileMenuBtnElement.addEventListener('click',toggleMobileMenu);
+
